d41b575b31e1ca66412d3c77ea2f3c15ee9242b5
Seth Delbridge <sadelbrid@gmail.com>
2015-11-27 11:51:23 -0500

    First

---
+++ app/src/main/java/com/delbridge/seth/simplelauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package com.delbridge.seth.simplelauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Seth on 11/27/15.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/com/delbridge/seth/simplelauncher/Home.java
@@ -0,0 +1,29 @@
+package com.delbridge.seth.simplelauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Home extends Activity {
+    static boolean locked = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(locked)
+            setContentView(R.layout.activity_home2);
+        else
+            showApps(null);
+    }
+
+    public void showApps(View v){
+        locked = false;
+        Intent i = new Intent(this, AppsList.class);
+        startActivity(i);
+    }
+}

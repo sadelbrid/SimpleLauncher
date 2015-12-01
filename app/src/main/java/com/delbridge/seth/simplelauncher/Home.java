5a673090258d365f88631a150cd56ccfddcaf1f0
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2015-12-01 01:51:45 -0500

    getting location and sending the cordinates and gmaps url in the email - sachindayl

--- app/src/main/java/com/delbridge/seth/simplelauncher/Home.java
+++ app/src/main/java/com/delbridge/seth/simplelauncher/Home.java
@@ -3,6 +3,7 @@ package com.delbridge.seth.simplelauncher;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -17,10 +18,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 
-public class Home extends Activity {
+public class Home extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private static int REQUEST_CODE_RECOVER_PLAY_SERVICES = 200;
     EditText password;
     static boolean locked = true;
     static String key = "1234";
@@ -30,6 +37,11 @@ public class Home extends Activity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private boolean mRequestingLocationUpdates = false;
+    private String latitude = "";
+    private String longitude = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +49,19 @@ public class Home extends Activity {
         startService(new Intent(Home.this, UpdateService.class));
         setContentView(R.layout.activity_home2);
 
+        if (checkGooglePlayServices()) {
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
+
+
 //        if(locked)
 //            setContentView(R.layout.activity_home2);
 //        else
 //            showApps(null);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
         password = (EditText) findViewById(R.id.passwordInput);
 
         password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -109,6 +127,7 @@ public class Home extends Activity {
                 //send email
                 Log.i("emailalert", "sending email");
                 sendEmail();
+
             }
         }
     }
@@ -132,7 +151,7 @@ public class Home extends Activity {
                     /**send email to the given recipient
                      */
                     sender.sendMail("LockScreen Alert ",
-                            "Someone tried to access your mobile device.",
+                            "Someone tried to access your mobile device.\n Your device is located at\n Latitude: " + latitude + " Longitude: " + longitude + "\n http://maps.google.com/maps?q=" + latitude + "," + longitude,
                             "androidtestingosu@gmail.com",
                             "sachinda.yl@gmail.com");
                 } catch (Exception e) {
@@ -146,24 +165,33 @@ public class Home extends Activity {
 
     }
 
+
     @Override
     public void onStart() {
         super.onStart();
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Home Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.delbridge.seth.simplelauncher/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        if (client != null) {
+            client.connect();
+            Action viewAction = Action.newAction(
+                    Action.TYPE_VIEW, // TODO: choose an action type.
+                    "Home Page", // TODO: Define a title for the content shown.
+                    // TODO: If you have web page content that matches this app activity's content,
+                    // make sure this auto-generated web page URL is correct.
+                    // Otherwise, set the URL to null.
+                    Uri.parse("http://host/path"),
+                    // TODO: Make sure this auto-generated app deep link URI is correct.
+                    Uri.parse("android-app://com.delbridge.seth.simplelauncher/http/host/path")
+            );
+            AppIndex.AppIndexApi.start(client, viewAction);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 
     @Override
@@ -183,6 +211,109 @@ public class Home extends Activity {
                 Uri.parse("android-app://com.delbridge.seth.simplelauncher/http/host/path")
         );
         AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
+        if (client != null) {
+            client.disconnect();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_RECOVER_PLAY_SERVICES) {
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!client.isConnecting() &&
+                        !client.isConnected()) {
+                    client.connect();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Google Play Services must be installed.",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    public boolean checkGooglePlayServices() {
+        int checkGooglePlayServices = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (checkGooglePlayServices != ConnectionResult.SUCCESS) {
+        /*
+		* Google Play Services is missing or update is required
+		*  return code could be
+		* SUCCESS,
+		* SERVICE_MISSING, SERVICE_VERSION_UPDATE_REQUIRED,
+		* SERVICE_DISABLED, SERVICE_INVALID.
+		*/
+            GooglePlayServicesUtil.getErrorDialog(checkGooglePlayServices,
+                    this, REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(20000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                client);
+        if (mLastLocation != null) {
+            latitude = String.valueOf(mLastLocation.getLatitude());
+            longitude = String.valueOf(mLastLocation.getLongitude());
+
+            //Toast.makeText(this, "Latitude:" + latitude +", Longitude:"+ longitude ,Toast.LENGTH_LONG).show();
+
+        }
+        mRequestingLocationUpdates = true;
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                client, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        //Toast.makeText(this, "Latitude:" + mLastLocation.getLatitude()+", Longitude:"+mLastLocation.getLongitude(),Toast.LENGTH_LONG).show();
+        latitude = String.valueOf(mLastLocation.getLatitude());
+        longitude = String.valueOf(mLastLocation.getLongitude());
+
+    }
+
+    protected void stopLocationUpdates() {
+        if (client != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    client, this);
+        }
     }
 }
